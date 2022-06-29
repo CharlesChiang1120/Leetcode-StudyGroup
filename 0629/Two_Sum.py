4e111bd8d3b8ebb673133a8835d802ce50d351f5
Charles <charleschiang2343@gmail.com>
2022-06-29 21:33:59 +0800

    V3

--- 0629/Two_Sum.py
+++ 0629/Two_Sum.py
@@ -29,3 +29,16 @@ class Solution:
                     pass 
                 else:
                     return [j, nums.index(hash_table[nums[j]] )]
+
+
+    # V3
+
+    def twoSum(self, nums: List[int], target:int) -> List[int]:
+            values = {}
+            for idx, element in enumerate(nums):
+                check = target-element 
+                if check in values:
+                   return [values[check], idx]
+                else:
+                    values[element] = idx
+                
