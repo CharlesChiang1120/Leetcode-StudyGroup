14f7e073b91a30e28f127d59d3be03341b15f7a7
Charles <charleschiang2343@gmail.com>
2022-06-29 21:19:20 +0800

    V2

--- 0629/Two_Sum.py
+++ 0629/Two_Sum.py
@@ -1,4 +1,5 @@
 class Solution:
+    # V1
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # BigO(n^2)
         # Go through all elements
@@ -8,3 +9,23 @@ class Solution:
                     return [i, j]     
                 else:
                     pass
+
+
+
+
+    # V2
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {}
+        
+        # calculate target minus elements in nums
+        for i in nums:
+            hash_table[i] = target - i
+
+        for j in range(0, len(nums)):
+            if hash_table[nums[j]] in nums:
+
+                # [3, 2, 4] return [0, 0] so need to pass
+                if j == nums.index(hash_table[nums[j]]):
+                    pass 
+                else:
+                    return [j, nums.index(hash_table[nums[j]] )]
