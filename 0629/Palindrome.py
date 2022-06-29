d475ce227893f48f9a7912f69a6fbfb6e26396f0
Charles <charleschiang2343@gmail.com>
2022-06-29 21:51:51 +0800

    V2

--- 0629/Palindrome.py
+++ 0629/Palindrome.py
@@ -1,6 +1,22 @@
 class Solution:
+    # convert to string
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
         else:
             return str(x) == str(x)[::-1]
+
+
+    # insert each digit into list
+    # compare list and reverse list
+    def isPalindrome(self, x: int) -> bool:
+        a = []
+        if x < 0:
+            return False
+        else:
+            while x >=  10:
+                a.append(x % 10)
+                x = x // 10
+            a.append(x)
+            
+            return a == a[::-1]
